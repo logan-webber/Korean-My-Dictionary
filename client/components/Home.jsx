77ec93e63a6269312bf0215498cd48d195d449ed
Logan Webber <logan.webber@hotmail.com>
2021-03-29 14:02:29 +1300

    added minor css so home page isnt a complete eyesore

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,9 +10,13 @@ function Home() {
 		<>
 			<h1 className='title'>Welcome to My Dictionary! </h1>
 			<h2 className='title'>Please select one of the following options</h2>
-			<Link to='/listofwords'>Click here to view your words</Link>
-			<Link to='/addword'>Click here to add a new word</Link>
-			
+			<div className='link'>
+				<Link to='/listofwords'>Click here to view your words</Link>
+			</div>
+			<div className='link'>
+				<br />
+				<Link to='/addword'>Click here to add a new word</Link>
+			</div>
 		</>
 	)
 }
