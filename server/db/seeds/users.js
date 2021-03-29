3eec267f2e4c687fa5be59c1916ff0046c618b4b
Logan Webber <logan.webber@hotmail.com>
2021-03-29 17:55:05 +1300

    filled the seeds with an admin and non admin example

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -1,13 +1,12 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { id: 1, username: 'admin', first_name: 'Admin', last_name: 'Admin', password: 'admin123', admin: true, time: "1616993655" },
+        { id: 2, username: 'user', first_name: 'Logan', last_name: 'Webber', password: 'yeet', admin: false, time: "1616993655" },
       ]);
     });
 };
