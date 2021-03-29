9349237f4c2591550d753dc7f291d1be8ee5045e
Logan Webber <logan.webber@hotmail.com>
2021-03-29 18:07:35 +1300

    reformed users seed so it actually works

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -1,12 +1,26 @@
+const { generateHash } = require('authenticare/server')
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return Promise.all(
+        [
+     
         { id: 1, username: 'admin', first_name: 'Admin', last_name: 'Admin', password: 'admin123', admin: true, time: "1616993655" },
         { id: 2, username: 'user', first_name: 'Logan', last_name: 'Webber', password: 'yeet', admin: false, time: "1616993655" },
-      ]);
-    });
-};
+        ].map(user => {
+          return generateHash(user.password)
+          .then(hash => {
+            console.log(hash)
+            user.password = hash
+            return user
+          })
+        }))
+        .then(users => {
+          return knex('users').insert(users)
+        })
+    })
+}
+    
