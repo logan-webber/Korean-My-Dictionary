5e41b615f6fbe147801c8d169777fdfebc1f016b
Logan Webber <logan.webber@hotmail.com>
2021-03-29 17:50:30 +1300

    created seeds and migrations for auth and filled the migration

---
+++ server/db/migrations/20210329174817_users.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', table => {
+        table.increments('id')
+        table.string('username')
+        table.string('first_name')
+        table.string('last_name')
+        table.string('password')
+        table.boolean('admin')
+        table.bigint('time')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users')
+};
