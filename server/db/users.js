b52033aacf23e3afcce6e88113831d98aa4296d2
Logan Webber <logan.webber@hotmail.com>
2021-03-30 14:35:04 +1300

    added users db functions

---
+++ server/db/users.js
@@ -0,0 +1,16 @@
+const connection = require('./connection')
+
+function getUsers(db = connection) {
+    return db('users').select()
+}
+
+function addUsers(user, db = connection) {
+    return db('users')
+        .insert(user, 'id')
+        .then(ids => ids[0])
+}
+
+module.exports = {
+    getUsers,
+    addUsers
+}
