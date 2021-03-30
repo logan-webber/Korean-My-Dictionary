c4343d21580f9f2e7bbccbf0a7196010149647b4
Logan Webber <logan.webber@hotmail.com>
2021-03-30 14:31:20 +1300

    added auth route

---
+++ server/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { applyAuthRoutes } = require('authenticare/server')
+
+const { userExists,
+    getUserByUsername,
+    createUser } = require('../db/auth')
+
+const router = express.Router()
+
+applyAuthRoutes(router, {
+    userExists,
+    getUserByName: getUserByUsername,
+    createUser
+})
+
+module.exports = router
