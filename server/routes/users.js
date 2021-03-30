db9c2bebd0bdd31525e7d3821d9acbcaa4466f9e
Logan Webber <logan.webber@hotmail.com>
2021-03-30 14:32:34 +1300

    added users routes

---
+++ server/routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const db = require('../db/users')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    db.getUsers()
+        .then(results => {
+            res.json(results)
+            return null
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Feelsbadman' })
+        })
+})
+
+
+module.exports = router
