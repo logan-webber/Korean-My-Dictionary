09928e04694bfcad034da3d6563726f1fcc3678f
Logan Webber <logan.webber@hotmail.com>
2021-04-08 15:38:55 +1200

    added login and signup components to app jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import AddWord from './AddWord'
 import ListOfWords from './ListOfWords'
 import Home from './Home'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import SignUp from './SignUp'
+import login from './login'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/listofwords' exact component={ListOfWords} />
         <Route path='/addword' exact component={AddWord} />
+        <Route path='/signup' exact component={SignUp} />
+        <Route path='/login' exact component={login} />
       </Router>
     </>
   )
