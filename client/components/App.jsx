688f35dd2bc2d43182203f061aa9b85e8176841b
Logan Webber <logan.webber@hotmail.com>
2021-04-13 11:45:39 +1200

    added auth to app jsx for extra security

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 import AddWord from './AddWord'
 import ListOfWords from './ListOfWords'
 import Home from './Home'
 import SignUp from './SignUp'
 import login from './login'
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Route path='/' exact component={Home} />
-        <Route path='/listofwords' exact component={ListOfWords} />
-        <Route path='/addword' exact component={AddWord} />
-        <Route path='/signup' exact component={SignUp} />
-        <Route path='/login' exact component={login} />
-      </Router>
-    </>
-  )
+function App({ auth }) {
+	return (
+		<>
+			<Router>
+
+				<Route path='/' exact component={Home} />
+				<Route path='/signup' exact component={SignUp} />
+				<Route path='/login' exact component={login} />
+				{auth.isAuthenticated &&
+					<>
+						<Route path='/listofwords' exact component={ListOfWords} />
+						<Route path='/addword' exact component={AddWord} />
+					</>
+				}
+			</Router>
+		</>
+	)
 }
 
-export default App
+const mapStateToProps = ({ auth }) => {
+	return {
+		auth
+	}
+}
+
+
+export default connect(mapStateToProps)(App)
