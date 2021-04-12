265a922473d173efaf34c07ccecea7f0141670c2
Logan Webber <logan.webber@hotmail.com>
2021-04-12 12:17:01 +1200

    added auth to home page to render certain things

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 
-function Home() {
+function Home(props) {
 
 
 	return (
@@ -11,25 +11,33 @@ function Home() {
 			<h1 className='title'>Welcome to My Dictionary! </h1>
 			<h2 className='title'>Please select one of the following options</h2>
 			<div className='link'>
+				<br />
 				<Link to='/listofwords'>Click here to view your words</Link>
 			</div>
 			<div className='link'>
 				<br />
 				<Link to='/addword'>Click here to add a new word</Link>
 			</div>
+			{!props.auth.isAuthenticated &&
+			<>
 			<div className='link'>
+				<br />
 				<Link to='/signup'>Click here to sign up</Link>
 			</div>
 			<div className='link'>
+				<br />
 				<Link to='/login'>Click here to login</Link>
 			</div>
+			</>
+}
 		</>
 	)
 }
 
 function mapStateToProps(globalState) {
 	return {
-		words: globalState.words
+		words: globalState.words,
+		auth: globalState.auth
 	}
 }
 
