d8a1267e69b26afc25f3140e59967d3e3bd743a7
Logan Webber <logan.webber@hotmail.com>
2021-04-15 12:11:55 +1200

    reformatted words table to work for new features

--- server/db/migrations/20210217192614_words.js
+++ server/db/migrations/20210217192614_words.js
@@ -4,6 +4,8 @@ exports.up = function (knex) {
         table.increments('id').primary()
         table.string('word')
         table.string('meaning')
+        table.integer('user_id')
+        table.integer('language_id')
     })
 
 };
