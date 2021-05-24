152225694e2086a5cd93cd82b402796f1b587b5e
Logan Webber <logan.webber@hotmail.com>
2021-05-24 13:20:58 +1200

    changed korean words to be capitalized

--- server/db/seeds/words.js
+++ server/db/seeds/words.js
@@ -4,10 +4,10 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('words').insert([
-        { id: 1, word: 'annyeonghaseyo', meaning: 'Hello', language_id: 2, user_id: 2 },
-        { id: 2, word: 'gamsahabnida', meaning: 'Thank you', language_id: 2, user_id: 2  },
-        { id: 3, word: 'ppang', meaning: 'Bread', language_id: 2, user_id: 2  },
-        { id: 4, word: 'sarang', meaning: 'love', language_id: 2, user_id: 1}
+        { id: 1, word: 'Annyeonghaseyo', meaning: 'Hello', language_id: 2, user_id: 2 },
+        { id: 2, word: 'Gamsahabnida', meaning: 'Thank you', language_id: 2, user_id: 2  },
+        { id: 3, word: 'Ppang', meaning: 'Bread', language_id: 2, user_id: 2  },
+        { id: 4, word: 'Sarang', meaning: 'Love', language_id: 2, user_id: 1}
       ]);
     });
 };
