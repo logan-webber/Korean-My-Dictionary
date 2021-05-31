49c632c235b207a3b449142bc3243f90bbd02f5c
Logan Webber <logan.webber@hotmail.com>
2021-05-31 18:10:56 +1200

    updated kanban

--- client/components/AddWord.jsx
+++ client/components/AddWord.jsx
@@ -57,7 +57,7 @@ function AddWord(props, auth) {
 						<label  >
 							<input className='new-word' type='text' name='word' placeholder='New word' onChange={(e) => handleChange(e)} />
 							<input className='new-word' type='text' name='meaning' placeholder='New meaning' onChange={(e) => handleChange(e)} />
-						
+							
 						</label>
 						<button type='submit'>Add the new word</button>
 					</form>
