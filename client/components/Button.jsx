1df79a01d64ac69ed3b084305726e81c849ad0c6
Logan Webber <logan.webber@hotmail.com>
2021-06-01 18:37:08 +1200

    checkpoint reached perhaps dont need button jsx

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+// import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import { connect } from 'react-redux'
 
-function Button(props) {
-    return (
-        <>
+// function Button(props) {
+//     return (
+//         <>
 
-        </>
-    )
+//         </>
+//     )
 
-}
-
-
+// }
 
 
 
 
 
-const mapStateToProps = ({ auth }) => {
-    return {
-        auth,
-    }
-}
 
-export default connect(mapStateToProps)(Button)
+
+// const mapStateToProps = ({ auth }) => {
+//     return {
+//         auth,
+//     }
+// }
+
+// export default connect(mapStateToProps)(Button)
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,10 +7,11 @@ import Dropdown from './Dropdown'
 function Nav(props) {
 
 	const [click, setClick] = useState(false)
+	const [dropdown, setDropdown] =useState(false)
 
 	const handleClick = () => setClick(!click)
 		
-	
+	const closeMobileMenu = () => setClick(false)
 
 	return (
 		<>
@@ -24,17 +25,25 @@ function Nav(props) {
 				<div className='menu-icon' onClick={handleClick}>
 					<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
 				</div>
+				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+					<li className='nav-item'>
+						<Link to='/listofwords' className='nav-links' onClick={closeMobileMenu}>
+							Your Words
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/yourdictionary' className='nav-links' onClick={closeMobileMenu}>
+							Your dictionary <i className='fas fa-caret-down' />
+						</Link>
+						{dropdown && <Dropdown/>}
+					</li>
+				</ul>
+				<Button />
 			</nav>
 		</>
 	)
 
 }
-
-
-
-
-
-
 
 const mapStateToProps = ({ auth }) => {
 	return {
