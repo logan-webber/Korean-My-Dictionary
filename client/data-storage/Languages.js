e9aa2f9adc3f99940e33c960c0f6af3cdf04c907
Logan Webber <logan.webber@hotmail.com>
2021-06-02 11:36:41 +1200

    created languages js to store language data

---
+++ client/data-storage/Languages.js
@@ -0,0 +1,12 @@
+export const Languages = [
+  {
+    language: 'English',
+    id: 1,
+    cName= 'dropdown-link'
+  },
+  {
+    language: 'Korean',
+    id: 2,
+    cName= 'dropdown-link'
+  }
+]
