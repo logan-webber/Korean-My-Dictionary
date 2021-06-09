50ed3021dd73602574ba8b52888b8ec9e896c273
Logan Webber <logan.webber@hotmail.com>
2021-06-09 16:01:24 +1200

    added images to home jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-function Home({auth}) {
+function Home({ auth }) {
+
+	const images = [
+		{ url: '/images/book-closeup-1.jpg', description: 'book closeup' },
+		{ url: '/images/man-with-book-1.jpg', description: 'man reading book' },
+		{ url: '/images/person-reading-1.jpg', description: 'person reading' }
+	]
+
+	const [currentImage, setCurrentImage] = useState(0)
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			if (currentImage == adverts.length - 1) {
+				setCurrentImage(0)
+			} else {
+				setCurrentImage(currentImage + 1);
+			}
+		}, 10000);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [currentImage]);
 
 	return (
 		<>
 			<h1 className='title'>Welcome to My Dictionary! </h1>
+			<>
+				<div className='main-images'>
+					{images.map((image, i) => (
+						<img
+							key={i}
+							src={image.url}
+							alt={image.description}
+							className='current-image'
+						/>
+					))}
+				</div>
+			</>
 			{auth.isAuthenticated &&
 				<>
-				
+
 				</>
 			}
 			{!auth.isAuthenticated &&
 				<>
-			
+
 				</>
 
 			}
