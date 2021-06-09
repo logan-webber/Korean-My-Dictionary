cd0099d7e5c2e185b6998573d39b3495485175ff
Logan Webber <logan.webber@hotmail.com>
2021-06-09 15:26:22 +1200

    cleaned out home jsx of unessersary items

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,30 +7,14 @@ function Home({auth}) {
 	return (
 		<>
 			<h1 className='title'>Welcome to My Dictionary! </h1>
-			<h2 className='title'>Please select one of the following options</h2>
 			{auth.isAuthenticated &&
 				<>
-					<div className='link'>
-						<br/>
-						<Link to='/listofwords'>Click here to view your words</Link>
-					</div>
-					<div className='link'>
-						<br/>
-						<Link to='/addword'>Click here to add a new word</Link>
-					</div>
+				
 				</>
 			}
 			{!auth.isAuthenticated &&
 				<>
-					<div className='link'>
-						<br />
-						<Link to='/login'>Click here to login</Link>
-		
-					</div>
-					<div className='link'>
-						<br />
-						<Link to='/signup'>Click here to sign up</Link>
-					</div>
+			
 				</>
 
 			}
